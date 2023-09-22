f3516caac23a6b518ec7eb86e865a154f25027dd
Levi <122761207+Levizada@users.noreply.github.com>
2023-09-22 18:08:42 -0300

    Add files via upload

---
+++ Carlinhos.py
@@ -0,0 +1,74 @@
+print("Jogo Do Carlinhos (Voce Conhece o Grande Rei?)")
+
+while True:
+    Carlinhos = int(input("O carlinhos Bebe?\n (1)Leite. \n(2)Esperma.\n(3)Agua."))
+    
+    if Carlinhos == 1:
+        print("Carlinhos Esta Triste Com Voce!")
+
+    elif Carlinhos == 2:
+        print("Voce e um Grande Fã Do Mestre Carlinhos!!")
+        break
+
+    elif Carlinhos == 3:
+        print("Carlinhos Esta Triste Com Voce!")
+    
+    else:
+        print("Responda O Carlinhos!!!!")
+
+print("Segunda Fase, É Bom Que Esteja Preparado para Grande e Imensa Dalva")
+
+while True:
+    Dalva = int(input("Fase 2: A Dalva é Uma?\n (1)Estrupada.\n (2)Mulher De respeito.\n (3)Vagabunda."))
+
+    if Dalva == 1:
+        print("Ta Maluco Porra Tem que fazer Direito")
+
+    elif Dalva == 2:
+        print("Burro Da Porra Dalva te Odeia")
+
+    elif Dalva == 3:
+        print("Parabens!! Voce e um grande conhecedor Dos Contos do Mestre Carlinhos")
+        break
+    else:
+        print("So 3 Perguntas e Voce Consegue errar? Patetico")
+
+print("Terceira Fase, Se Prepare Pois a Cleide Esta Sem Calcinha")
+
+while True:
+    Carlinhos1 = int(input("Fase 3: Qual o Time De Coração Do Mestre Carlinhos?\n (1)Flamengo.\n (2)Corinthians.\n (3)Vasco.\n (4)Bahia."))
+
+    if Carlinhos1 == 1:
+        print("Errado!!")
+
+    elif Carlinhos1 == 2:
+        print("Parabens Carlinhos ama o Cássio e Seu Sonho e Pegar Em muitas Bolas Como ele!!")
+        break
+    if Carlinhos1 == 3:
+        print("Ta Achando que ele quer um time Falido?")
+
+    if Carlinhos1 == 4:
+        print("Errado!!")
+
+    else:
+        print("Responda Imediatamente")
+
+print("Quarta Fase, Agora Iremos Aumentar a Dificuldade")
+
+while True:
+    Carlinhos2 = int(input("Fase 4: Aqui Iremos Falar Sobre Nosso Grande Mestre\n Qual a Idade Do Nosso Mestre (1)62.\n (2)54.\n (3)75."))
+
+    if Carlinhos2 == 1:
+        print("Parabens!! Voce Gosta mesmo do mestre em =)")
+        break        
+
+    elif Carlinhos2 == 2:
+        print("Burro Animal Vai se fuder")
+
+    elif Carlinhos == 3:
+        print("Errou Feio Ele nao e Tao Velho Assim!")
+
+    else:
+        print("Responda Imediatamente!")
+
+print("Parabens Voce Terminou E Respondeu Todas as Perguntas Corretamente Obrigado Por Participar Do Jogo do Carlinhos =).")
